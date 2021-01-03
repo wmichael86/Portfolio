09fe51a08b883563025e6772ecad24aa1087d2e8
Michael Lewis <wmichael.lewis@outlook.com>
2021-01-03 04:29:37 -0800

    gh pages test

--- src/components/main.js
+++ src/components/main.js
@@ -3,23 +3,63 @@ import Navbar from './Navbar';
 import Header from './header';
 
 import Particles from 'react-particles-js';
+import { makeStyles } from "@material-ui/styles";
 
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute",
+    opacity: "0.3"
+  }
+});
 export const Home = () => {
+    const classes = useStyles()
     return (
         <>
             
             <Navbar />
             <Header />
-            <Particles 
-                params={{
-                    particles:{
-                        number: {
-                            value: 45
-                        }
-                    }
-                }}
-            />
-        </>
+            <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "tomato"
+              }
+            },
+            size: {
+              value: 8,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 6,
+                size_min: 0.1,
+                sync: true
+              }
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true
+              }
+            }
+          }
+        }}
+      />
+    </>
     )
 }
 
